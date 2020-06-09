1a63370a90888168de6792691a5ff7cfe22daf42
Rik <rik@huijzer.xyz>
2020-06-09 15:25:48 +0200

    Add combined ranges for three regexes

--- src/evalmd.jl
+++ src/evalmd.jl
@@ -1,31 +1,17 @@
-# Using find on strings to avoid using a non-trivial parser.
-# For example, it is non-trivial to parse ```a `x` b```.
+export ranges
 
-inline_math_regex = r"\$(?![\(|\$]).{1}\$"
-display_math_regex = r"\$\$[^\$]+?\$\$"
-eval_regex = r"\$\(.+?\)"
+display_regex = r"\$\$[^\$]+?\$\$" # For example, $$ x $$.
+eval_regex = r"\$\(.+?\)" # For example, $(x).
+inline_regex = r"\$(?![\(|\$]).{1}\$(?!\$)" # For example, $x$.
 
-function findallstr(pattern::AbstractString, s)
-	ranges::Array{UnitRange,1} = []
-	result = 0:0
-	while (result = findnext(pattern, s, result.stop + 1)) != nothing
-		push!(ranges, result)
-	end
-	ranges
-end
-findallstr(pattern::Char, s::AbstractString) = findallstr(string(pattern), s)
-export findallstr
+match2range(m::RegexMatch)::UnitRange =
+	m.offset:m.offset + (length(m.match) - 1)
 
-function ranges(md::AbstractString)::Array{Range,1}
-	ranges = []
-	dollar_hits = findallstr("\$", md)
-	display_hits = findallstr("\$\$", md)
-	foreach(i -> push!(ranges, display_hits[2*i], 
-	for i in 1:display_hits/2
-			
-	end
-	ranges
-end
+ranges(s::AbstractString, rx::Regex)::Array{UnitRange,1} = 
+	map(match2range, eachmatch(rx, s))
+
+ranges(md::AbstractString)::Array{UnitRange,1} =
+	sort(vcat(map(rx -> ranges(md, rx), [inline_regex, display_regex, eval_regex])...))
 
 function splitmd(md::AbstractString)::Array{AbstractString,1}
 	rs = ranges(md)
--- test/test_evalmd.jl
+++ test/test_evalmd.jl
@@ -2,5 +2,6 @@ using GenerateMarkdown
 using Test
 
 @testset "evalmd" begin
-	@test findallstr("zz", "zzabzz") == [1:2, 5:6]
+	@test ranges(raw"a $x$ and $y$", GenerateMarkdown.inline_regex) == [3:5, 11:13]
+	@test ranges(raw"$$x$$, $y$ and $(z)") == [1:5, 8:10, 16:19]
 end
