bc8574c68353bb2a4c857f3842d8f6bd5ccc243c
Rik <rik@huijzer.xyz>
2020-08-15 19:04:19 +0200

    Set optlevel to 0

--- src/GenTeX.jl
+++ src/GenTeX.jl
@@ -2,7 +2,7 @@ module GenTeX
 
 # Most time is spend on calling LaTeX, so the compiler shouldn't spend much time on optimizing.
 if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
-    @eval Base.Experimental.@optlevel 1
+    @eval Base.Experimental.@optlevel 0
 end
 
 include("evalmd.jl")
