6d93ca5a2a18d0441017f9b617262cc53d68e3ad
Rik <rik@huijzer.xyz>
2020-08-15 19:09:20 +0200

    Cut setting optlevel; doesnt help on site build

--- src/GenTeX.jl
+++ src/GenTeX.jl
@@ -1,10 +1,5 @@
 module GenTeX
 
-# Most time is spend on calling LaTeX, so the compiler shouldn't spend much time on optimizing.
-if isdefined(Base, :Experimental) && isdefined(Base.Experimental, Symbol("@optlevel"))
-    @eval Base.Experimental.@optlevel 0
-end
-
 include("evalmd.jl")
 include("cache.jl")
 include("example.jl")
