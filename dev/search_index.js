615a67d50da68a108f6793569acc8aa53f9f241e
Documenter.jl <documenter@juliadocs.github.io>
2020-09-24 13:30:10 +0000

    build based on 19f911e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GenTeX.jl","page":"Home","title":"GenTeX.jl","text":"","category":"section"},{"location":"#Public-methods","page":"Home","title":"Public methods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Modules = [GenDoc]\nPrivate = false","category":"page"},{"location":"#Private-methods","page":"Home","title":"Private methods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Modules = [GenDoc]\nPublic = false","category":"page"}]
+}
